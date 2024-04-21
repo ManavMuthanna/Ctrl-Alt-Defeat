2427d977ffa7dacc09e45f6109566e9e00d3c60e
gptsarthak <sarthakgpt95@gmail.com>
2024-04-21 12:27:29 +0530

    feat: deploy using streamlit

---
+++ streamlit/data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+@st.cache_data()
+def load_data():
+    files = ['Bangalore.csv', 'Delhi.csv', 'Chennai.csv', 'Hyderabad.csv', 'Mumbai.csv', 'Kolkata.csv']
+    data_frames = []
+    for file in files:
+        df = pd.read_csv(f'../data/{file}').replace(9, np.nan).dropna()
+        df['Price'] = df['Price']/100000
+        data_frames.append(df)
+    return data_frames
