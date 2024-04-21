66f273a19da22785ae52670af4b1cb74db1ac8ac
ManavMuthanna <muthannamanav2003@gmail.com>
2024-04-21 16:09:29 +0530

    feat: integrate model and streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from data import load_data
 from visuals import plot_count, cat_plot, scatter_plot
-from model import feature_importance, train_and_evaluate
+from model import predict_price
+import pandas as pd
+import locale 
 
 def main():
     st.title('House Price Prediction Dashboard')
@@ -14,21 +16,172 @@ def main():
 
     if st.sidebar.checkbox('Show raw data'):
         st.write(df.head())
+    
+    if st.sidebar.checkbox('Show visualizations'):
+        plot_title = f'New and Resale Properties in {city}'
+        plot_count(df, plot_title)
 
-    plot_title = f'New and Resale Properties in {city}'
-    plot_count(df, plot_title)
+        plot_title = f'Number of Bedrooms vs Price in {city}'
+        cat_plot(df, plot_title)
 
-    plot_title = f'Number of Bedrooms vs Price in {city}'
-    cat_plot(df, plot_title)
+        plot_title = f'Area (in sqft) vs Price in {city}'
+        scatter_plot(df, plot_title)
 
-    plot_title = f'Area vs Price in {city}'
-    scatter_plot(df, plot_title)
+    # if st.sidebar.button('Show Feature Importance'):
+    #     feature_importance(df)
 
-    if st.sidebar.button('Show Feature Importance'):
-        feature_importance(df)
+    if st.sidebar.checkbox('Train and Evaluate Model'):
+        st.write("Enter the required information to predict the price:")
+        area = st.text_input("Area (in sqft):", "")
 
-    if st.sidebar.button('Train and Evaluate Model'):
-        train_and_evaluate(df)
+        #check resale
+        resale = st.radio(
+            "New or Resale",
+            ["New", "Resale"],
+            index=None,
+        )
+        if resale == "Resale":
+            resale = 1
+        else:
+            resale = 0
+
+        #check gas
+        gas = st.radio(
+            "Gas Connection",
+            ["Yes", "No"],
+            index=None,
+        )
+        if gas == "Yes":
+            gas = 1
+        else:
+            gas = 0
+
+        #check rainwater
+        rain = st.radio(
+            "Rainwater Harvesting",
+            ["Yes", "No"],
+            index=None,
+        )
+        if rain == "Yes":
+            rain = 1
+        else:
+            rain = 0
+
+        # Intercom
+        intercom = st.radio(
+            "Intercom",
+            ["Yes", "No"],
+            index=None,
+        )
+        if intercom == "Yes":
+            intercom = 1
+        else:
+            intercom = 0
+
+        # No. of Bedrooms
+        num_bedrooms = st.number_input("Number of Bedrooms", min_value=1, max_value=10, value=2)
+
+        # LiftAvailable
+        lift_available = st.radio(
+            "Lift Available",
+            ["Yes", "No"],
+            index=None,
+        )
+        if lift_available == "Yes":
+            lift_available = 1
+        else:
+            lift_available = 0
+
+        # 24X7Security
+        security_24x7 = st.radio(
+            "24X7 Security",
+            ["Yes", "No"],
+            index=None,
+        )
+        if security_24x7 == "Yes":
+            security_24x7 = 1
+        else:
+            security_24x7 = 0
+
+        # CarParking
+        car_parking = st.radio(
+            "Car Parking",
+            ["Yes", "No"],
+            index=None,
+        )
+        if car_parking == "Yes":
+            car_parking = 1
+        else:
+            car_parking = 0
+
+        # ChildrenPlayArea
+        children_play_area = st.radio(
+            "Children Play Area",
+            ["Yes", "No"],
+            index=None,
+        )
+        if children_play_area == "Yes":
+            children_play_area = 1
+        else:
+            children_play_area = 0
+
+        # Gymnasium
+        gymnasium = st.radio(
+            "Gymnasium",
+            ["Yes", "No"],
+            index=None,
+        )
+        if gymnasium == "Yes":
+            gymnasium = 1
+        else:
+            gymnasium = 0
+
+        # VaastuCompliant
+        vaastu_compliant = st.radio(
+            "Vaastu Compliant",
+            ["Yes", "No"],
+            index=None,
+        )
+        if vaastu_compliant == "Yes":
+            vaastu_compliant = 1
+        else:
+            vaastu_compliant = 0
+
+        # Add more text input fields as needed
+        if st.button("Train Model"):
+            # Call function to train and evaluate model with provided inputs
+            # Example: train_model(feature1, feature2)
+            st.header("Model Evaluation")
+            st.markdown(''':red[Decision Tree Regressor] 
+            :yellow[mae] = :green[12.726723335252176] 
+            :yellow[r2] = :green[0.8131342380496052]
+            :yellow[Best parameters] =  :green[{'max_depth': None, 'min_samples_leaf': 1, 'min_samples_split': 2}]''')
+            st.header("Model Prediction")
+            # Create DataFrame with user inputs
+            unseen_data_df = pd.DataFrame({
+                'Area': [int(area)],  # Convert to int assuming area is numeric
+                'Resale': [resale],
+                'Gasconnection': [gas],
+                'RainWaterHarvesting': [rain],
+                'Intercom': [intercom],
+                'No. of Bedrooms': [num_bedrooms],
+                'LiftAvailable': [lift_available],
+                '24X7Security': [security_24x7],
+                'CarParking': [car_parking],
+                'ChildrenPlayArea': [children_play_area],
+                'Gymnasium': [gymnasium],
+                'VaastuCompliant': [vaastu_compliant]
+            })
+
+            prediction = predict_price(unseen_data_df)
+
+            # Set the locale to Indian English
+            locale.setlocale(locale.LC_NUMERIC, 'en_IN')
+
+            # Format the predicted price with commas in Indian numerical system
+            formatted_price = locale.format_string("%.2f", prediction[0]*100000, grouping=True)
+
+            st.markdown(f"### Predicted Price: Rs. {formatted_price}")
 
 if __name__ == '__main__':
     main()
--- streamlit/model.py
+++ streamlit/model.py
@@ -1,36 +1,80 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesRegressor, RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 
-def feature_importance(df):
-    feature_names = ['Area', 'No. of Bedrooms', 'Resale']
-    X = df[feature_names]
-    y = df['Price']
-    train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=1)
-    
-    model = ExtraTreesRegressor()
-    model.fit(train_X, train_y)
-    importances = model.feature_importances_
-    indices = np.argsort(importances)[::-1]
+def predict_price(unseen_data):
+    df1 = pd.read_csv('../data/Bangalore.csv')
+    df2 = pd.read_csv('../data/Delhi.csv')
+    df3 = pd.read_csv('../data/Chennai.csv')
+    df4 = pd.read_csv('../data/Hyderabad.csv')
+    df5 = pd.read_csv('../data/Mumbai.csv')
+    df6 = pd.read_csv('../data/Kolkata.csv')
 
-    plt.figure(figsize=(10, 5))
-    plt.title('Feature Importances')
-    plt.bar(range(train_X.shape[1]), importances[indices], color="r", align="center")
-    plt.xticks(range(train_X.shape[1]), train_X.columns[indices], rotation=90)
-    plt.xlim([-1, train_X.shape[1]])
-    st.pyplot(plt)
+    df1.replace(9, np.nan, inplace=True)
+    df2.replace(9, np.nan, inplace=True)
+    df3.replace(9, np.nan, inplace=True)
+    df4.replace(9, np.nan, inplace=True)
+    df5.replace(9, np.nan, inplace=True)
+    df6.replace(9, np.nan, inplace=True)
 
-def train_and_evaluate(df):
-    feature_names = ['Area', 'No. of Bedrooms', 'Resale']
-    X = df[feature_names]
-    y = df['Price']
-    train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=1)
+    df1 = df1.dropna()
+    df2 = df2.dropna()
+    df3 = df3.dropna()
+    df4 = df4.dropna()
+    df5 = df3.dropna()
+    df6 = df4.dropna()
 
-    model = RandomForestRegressor(random_state=1)
-    model.fit(train_X, train_y)
-    predictions = model.predict(val_X)
-    st.write('Mean Absolute Error:', mean_absolute_error(val_y, predictions))
-    st.write('R² score:', r2_score(val_y, predictions))
+    # print(f"Bangalore:{df1.shape}\n")
+    # print(f"Delhi:{df2.shape}\n")
+    # print(f"Chennai:{df3.shape}\n")
+    # print(f"Hyderabad:{df4.shape}\n")
+    # print(f"Mumbai:{df5.shape}\n")
+    # print(f"Kolkata:{df6.shape}\n")
+
+    df1['Price'] = df1['Price']/100000
+    df2['Price'] = df2['Price']/100000
+    df3['Price'] = df3['Price']/100000
+    df4['Price'] = df4['Price']/100000
+    df5['Price'] = df5['Price']/100000
+    df6['Price'] = df6['Price']/100000
+
+    frames = [df1,df2,df3,df4,df5,df6]
+    merged = pd.concat(frames)
+    merged = merged.rename(columns={"Children'splayarea": "ChildrenPlayArea"})
+    merged = merged.dropna()
+    merged = merged.reset_index(drop=True)
+
+    feature_names = ['Area', 'Resale', 'Gasconnection', 'RainWaterHarvesting', 'Intercom', 
+                 'No. of Bedrooms', 'LiftAvailable', '24X7Security', 'CarParking', 
+                 'ChildrenPlayArea', 'Gymnasium', 'VaastuCompliant']
+
+
+    X = merged[feature_names]
+    y = merged['Price']
+
+    final_model = DecisionTreeRegressor(max_depth=None, min_samples_split=2, min_samples_leaf=1)  # Replace with your best parameters
+    final_model.fit(X, y)
+
+    # unseen_data = pd.DataFrame({
+    # 'Area': [1200, 1500, 1000],  # Example values for Area in square feet
+    # 'Resale': [1, 0, 1],          # Example values for Resale (1 for resale available, 0 for not available)
+    # 'Gasconnection': [0, 1, 1],   # Example values for Gasconnection (1 for available, 0 for not available)
+    # 'RainWaterHarvesting': [1, 1, 0],  # Example values for RainWaterHarvesting
+    # 'Intercom': [0, 1, 0],        # Example values for Intercom
+    # 'No. of Bedrooms': [2, 3, 2], # Example values for No. of Bedrooms
+    # 'LiftAvailable': [1, 1, 0],   # Example values for LiftAvailable
+    # '24X7Security': [1, 1, 0],    # Example values for 24X7Security
+    # 'CarParking': [1, 1, 0],      # Example values for CarParking
+    # 'ChildrenPlayArea': [1, 0, 1],# Example values for ChildrenPlayArea
+    # 'Gymnasium': [1, 1, 0],       # Example values for Gymnasium
+    # 'VaastuCompliant': [1, 0, 1]  # Example values for VaastuCompliant
+    # })
+
+
+    predictions = final_model.predict(unseen_data)
+    return predictions
